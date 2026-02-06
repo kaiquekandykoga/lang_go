b3203fe0e76f0aa8d0b2147896206c2c87fa9f33
Kaíque Kandy Koga <kaiquekandykoga@gmail.com>
2026-02-07 12:37:14 +1300

    add Go's shorthand variable declaration

--- go.go
+++ go.go
@@ -17,17 +17,24 @@ func main() {
 	fmt.Printf("[math] math.Sqrt(16) = %g \n", math.Sqrt(16))
 	fmt.Println("[math] math.Pi =", math.Pi)
 
+	fmt.Println("\nBEGIN add_0() and add_1()")
 	fmt.Println("add_0(3, 5) =", add_0(3, 5))
 	fmt.Println("add_1(3, 5) =", add_1(3, 5))
+	fmt.Println("END add_0() and add_1()")
 
+	fmt.Println("\nBEGIN swap()")
 	koga, kandy := swap("Koga", "Kandy")
 	fmt.Println("swap(\"Koga\", \"Kandy\") =", koga, kandy)
+	fmt.Println("END swap()")
 
+	fmt.Println("\nBEGIN split()")
 	x, y := split(17)
 	fmt.Println("split(17) =", x, y)
+	fmt.Println("END split()")
 
-	fmt.Println()
+	fmt.Println("\nBEGIN variables()")
 	variables()
+	fmt.Println("END variables()")
 }
 
 func add_0(a int, b int) int {
@@ -53,13 +60,14 @@ func split(sum int) (x, y int) {
 }
 
 func variables() {
-	fmt.Println("BEGIN variables()")
-
 	var i int
 	fmt.Println(i, c, python, java) // 0 false false false
 
 	var i_1, j_1 int = 1, 2
 	fmt.Println(i_1, j_1) // 1 2
 
-	fmt.Println("END variables()")
+	// Go's shorthand variable declaration
+	k := 3
+	csharp, ruby, php := true, false, "はい！"
+	fmt.Println(k, csharp, ruby, php) // 3 true false はい！
 }
