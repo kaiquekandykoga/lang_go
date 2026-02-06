a8a9f4b6ac11fa16084d307373b24ec90a1236fb
Kaíque Kandy Koga <kaiquekandykoga@gmail.com>
2026-02-07 12:06:31 +1300

    add math.Pi and format strings

--- go.go
+++ go.go
@@ -9,7 +9,8 @@ import (
 
 func main() {
 	fmt.Println("ありがとう！")
-	fmt.Println("time package = ", time.Now())
-	fmt.Println("math/rand package = ", rand.Intn(9))
-	fmt.Printf("math package = %g \n", math.Sqrt(16))
+	fmt.Println("[time] time.Now() =", time.Now())
+	fmt.Println("[math/rand] rand.Intn(9) =", rand.Intn(9))
+	fmt.Printf("[math] math.Sqrt(16) = %g \n", math.Sqrt(16))
+	fmt.Println("[math] math.Pi =", math.Pi)
 }
