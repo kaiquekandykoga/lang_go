b415234bbcc82718c9eefe2e0722d5d5f18ca92b
Kaíque Kandy Koga <kaiquekandykoga@gmail.com>
2026-02-07 12:42:38 +1300

    add basic_types function to go.go

--- go.go
+++ go.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/cmplx"
 	"math/rand"
 	"time"
 )
@@ -35,6 +36,10 @@ func main() {
 	fmt.Println("\nBEGIN variables()")
 	variables()
 	fmt.Println("END variables()")
+
+	fmt.Println("\nBEGIN basic_types()")
+	basic_types()
+	fmt.Println("END basic_types()")
 }
 
 func add_0(a int, b int) int {
@@ -70,4 +75,28 @@ func variables() {
 	k := 3
 	csharp, ruby, php := true, false, "はい！"
 	fmt.Println(k, csharp, ruby, php) // 3 true false はい！
+}
+
+func basic_types() {
+	/*
+				bool
+				string
+				int  int8  int16  int32  int64
+				uint uint8 uint16 uint32 uint64 uintptr
+				byte // alias for uint8
+				rune // alias for int32
+		 		    // represents a Unicode code point
+				float32 float64
+				complex64 complex128
+	*/
+
+	// Go's block of variable declarations
+	var (
+		ToBe   bool       = false
+		MaxInt uint64     = 1<<64 - 1
+		z      complex128 = cmplx.Sqrt(-5 + 12i)
+	)
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
 }
