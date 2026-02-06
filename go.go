428827274d0badf70177300e503b26b774b2a517
Kaíque Kandy Koga <kaiquekandykoga@gmail.com>
2026-02-07 12:18:18 +1300

    add swap function in go.go

--- go.go
+++ go.go
@@ -16,6 +16,9 @@ func main() {
 
 	fmt.Println("add_0(3, 5) =", add_0(3, 5))
 	fmt.Println("add_1(3, 5) =", add_1(3, 5))
+
+	koga, kandy := swap("Koga", "Kandy")
+	fmt.Println("swap(\"Koga\", \"Kandy\") =", koga, kandy)
 }
 
 func add_0(a int, b int) int {
@@ -26,4 +29,9 @@ func add_0(a int, b int) int {
 // can share a single type declaration
 func add_1(x, y int) int {
 	return x + y
+}
+
+// Go's multiple return values
+func swap(x, y string) (string, string) {
+	return y, x
 }
