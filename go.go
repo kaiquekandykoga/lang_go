ff6dfc70d335ad17d67a9014fdbaa470f7ea54a1
Kaíque Kandy Koga <kaiquekandykoga@gmail.com>
2026-02-07 12:14:25 +1300

    add add_0 and add_1 functions using different parameter declaration syntaxes int

--- go.go
+++ go.go
@@ -13,4 +13,17 @@ func main() {
 	fmt.Println("[math/rand] rand.Intn(9) =", rand.Intn(9))
 	fmt.Printf("[math] math.Sqrt(16) = %g \n", math.Sqrt(16))
 	fmt.Println("[math] math.Pi =", math.Pi)
+
+	fmt.Println("add_0(3, 5) =", add_0(3, 5))
+	fmt.Println("add_1(3, 5) =", add_1(3, 5))
+}
+
+func add_0(a int, b int) int {
+	return a + b
+}
+
+// Go's shorthand where consecutive parameters of the same type
+// can share a single type declaration
+func add_1(x, y int) int {
+	return x + y
 }
